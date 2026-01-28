02429d92368ca26abec6691ac7ccfd84c6dbf737
yuuCraft <yuucraft1298@proton.me>
2026-01-28 18:51:21 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.2 });
+
+reveals.forEach(el => observer.observe(el));
