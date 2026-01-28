230ea9805bed1e07880b0c06151b694a60119afc
yuuCraft <yuucraft1298@proton.me>
2026-01-28 18:57:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const reveals = document.querySelectorAll('.reveal');
+const targets = document.querySelectorAll('.reveal');
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
+const io = new IntersectionObserver(entries => {
+  entries.forEach(e => {
+    if (e.isIntersecting) {
+      e.target.classList.add('show');
     }
   });
 }, { threshold: 0.2 });
 
-reveals.forEach(el => observer.observe(el));
+targets.forEach(t => io.observe(t));
